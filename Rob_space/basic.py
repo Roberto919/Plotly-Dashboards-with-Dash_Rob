8c146e3526bef84c0b515bb94f6af4dd114abee3
Roberto919 <robertosysm@gmail.com>
2020-11-12 14:00:08 -0600

    Completing real data line charts lesson

--- Rob_space/basic.py
+++ Rob_space/basic.py
@@ -8,6 +8,7 @@
 import numpy as np
 import plotly.offline as pyo
 import plotly.graph_objs as go
+import pandas as pd
 
 
 
@@ -91,4 +92,27 @@ layout = go.Layout(
 
 fig = go.Figure(data=data, layout=layout)
 
-pyo.plot(fig, filename="line_chart.html")
+# pyo.plot(fig, filename="line_chart.html")
+
+
+
+## Creating chart with real data
+
+
+## Importing data and filtering
+df = pd.read_csv("../SourceData/nst-est2017-alldata.csv")
+
+df2 = df[df["DIVISION"] == "1"]
+
+df2.set_index("NAME", inplace=True)
+
+list_of_pop_col = [col for col in df2.columns if col.startswith("POP")]
+df2 = df2[list_of_pop_col]
+
+data = [go.Scatter(
+            x=df2.columns,
+            y=df2.loc[name],
+            mode="lines",
+            name=name) for name in df2.index]
+
+pyo.plot(data, filename="lineplot_real_data.html")

c1e638b0312942cce88ad30d6e55094c54edffe5
Roberto919 <robertosysm@gmail.com>
2020-11-12 13:08:53 -0600

    Completing line charts tutorial with random data

--- Rob_space/basic.py
+++ Rob_space/basic.py
@@ -1,4 +1,4 @@
-## MODULE CREATED FOR Section 3: Plotly Basics - Scatter Plots lesson
+## MODULE CREATED FOR Section 3: Plotly Basics
 
 
 
@@ -10,13 +10,18 @@ import plotly.offline as pyo
 import plotly.graph_objs as go
 
 
-## Creating random data
+
+
+
+## Creating scatter plot
+
+#### Creating random data for chart
 np.random.seed(42)
 random_x = np.random.randint(1, 101, 100)
 random_y = np.random.randint(1, 101, 100)
 
 
-## Crating plot
+#### Creating chart
 data = [
     go.Scatter(
         x=random_x,
@@ -42,4 +47,48 @@ layout = go.Layout(
 
 fig = go.Figure(data=data, layout=layout)
 
-pyo.plot(fig, filename="scatter.html")
+# pyo.plot(fig, filename="scatter.html")
+
+
+
+## Creating line plot
+
+#### Creating random data for chart
+np.random.seed(56)
+x_values = np.linspace(0, 1, 100)
+y_values = np.random.randn(100)
+
+
+
+#### Creating chart
+trace0 = go.Scatter(
+    x=x_values,
+    y=(y_values + 5),
+    mode="markers",
+    name="markers"
+)
+
+trace1 = go.Scatter(
+    x=x_values,
+    y=(y_values),
+    mode="lines",
+    name="mylines"
+)
+
+trace2 = go.Scatter(
+    x=x_values,
+    y=(y_values - 5),
+    mode="lines+markers",
+    name="my fav"
+)
+
+
+data = [trace0, trace1, trace2]
+
+layout = go.Layout(
+    title="Line Charts"
+)
+
+fig = go.Figure(data=data, layout=layout)
+
+pyo.plot(fig, filename="line_chart.html")

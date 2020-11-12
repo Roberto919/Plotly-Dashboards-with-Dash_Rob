5c2000ff6a9bb3f8ff04bd3a9d9f5e05a4d416b7
Roberto919 <robertosysm@gmail.com>
2020-11-12 12:55:08 -0600

    Solving exercise of Section 3: Plotly Basics - Scatter Plots lesson

---
+++ Rob_space/basic.py
@@ -0,0 +1,45 @@
+## MODULE CREATED FOR Section 3: Plotly Basics - Scatter Plots lesson
+
+
+
+
+
+## Imports
+import numpy as np
+import plotly.offline as pyo
+import plotly.graph_objs as go
+
+
+## Creating random data
+np.random.seed(42)
+random_x = np.random.randint(1, 101, 100)
+random_y = np.random.randint(1, 101, 100)
+
+
+## Crating plot
+data = [
+    go.Scatter(
+        x=random_x,
+        y=random_y,
+        mode="markers",
+        marker={
+            "size": 12,
+            "color": "rgb(51, 204, 153)",
+            "symbol": "pentagon",
+            "line": {"width": 2}
+        }
+    ),
+]
+
+layout = go.Layout(
+    title="Hello first plot",
+    xaxis={
+        "title":"MY X AXIS",
+    },
+    yaxis=dict(title="MY X AXIS"),
+    hovermode="closest"
+)
+
+fig = go.Figure(data=data, layout=layout)
+
+pyo.plot(fig, filename="scatter.html")
